aaf827d2724b136efebd0a88f130db268034b944
parlant <parlant@LAPTOP-5I9F3I0S>
2018-02-05 16:23:25 +0100

    [neo4j] connexion à la base

---
+++ project_poc_2017/src/com/project/poc/server/base/BaseNeo4j.java
@@ -0,0 +1,57 @@
+package com.project.poc.server.base;
+
+
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.TransactionWork;
+
+import static org.neo4j.driver.v1.Values.parameters;
+ 
+public class BaseNeo4j implements AutoCloseable
+{ 
+    private final Driver driver;
+
+    public BaseNeo4j( String uri )
+    {
+    	driver = GraphDatabase.driver( uri );
+    }
+
+    @Override
+    public void close() throws Exception
+    {
+        driver.close();
+    }
+
+    public void printGreeting( final String message )
+    {
+        try ( Session session = driver.session() )
+        {
+            String greeting = session.writeTransaction( new TransactionWork<String>()
+            {
+                @Override
+                public String execute( Transaction tx )
+                {
+                    StatementResult result = tx.run( "CREATE (a:Greeting) " +
+                                                     "SET a.message = $message " +
+                                                     "RETURN a.message + ', from node ' + id(a)",
+                            parameters( "message", message ) );
+                    return result.single().get( 0 ).asString();
+                }
+            } );
+            System.out.println( greeting );
+        }
+    }
+
+    public static void main( String... args ) throws Exception
+    {
+        try ( BaseNeo4j greeter = new BaseNeo4j( "bolt://obiwan2.univ-brest.fr:7687" ) )
+        {
+            greeter.printGreeting( "hello, world" );
+        }
+    }
+}
