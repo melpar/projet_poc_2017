b8b94fbf54c77f12322a987492bafe9d7de1d1d9
scawes <thomas.marescaux@hotmail.fr>
2018-02-06 15:31:13 +0100

    [ Mongo ] connexion

--- project_poc_2017/src/com/project/poc/server/base/BaseMongoDB.java
+++ project_poc_2017/src/com/project/poc/server/base/BaseMongoDB.java
@@ -15,19 +15,37 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class BaseMongoDB {
+	
+	private MongoClient mongoClient;
+	MongoDatabase db;
+	
 	public static void main(String[]args){
-		// Création de la connexion à MongoDB
-		MongoClientURI uri = new MongoClientURI("mongodb://obiwan2.univ-brest.fr");
-		MongoClient mongoClient = new MongoClient(uri);
-		// Notre base de données
-		MongoDatabase db = mongoClient.getDatabase("BDMongomaster041");
-		
-		MongoCollection<Document> collection = db.getCollection("oeuvres");
-		Document myDoc = collection.find().first();
-		System.out.println(myDoc);
+		BaseMongoDB mongo = new BaseMongoDB();
+		mongo.ouvrir();
+		mongo.test();
+		mongo.fermer();
 //		System.out.println(myDoc.toJson());
 		//System.out.println(artistes.findOne());
 		//DBObject artiste = artistes.findOne();
 		//System.out.println(artiste);
+	}
+	public void ouvrir(){
+		MongoClientURI uri = new MongoClientURI("mongodb://obiwan2.univ-brest.fr");
+		mongoClient = new MongoClient(uri);
+		db = mongoClient.getDatabase("BDMongomasterProjet041");
+	}
+	
+	public void fermer(){
+		mongoClient.close();
+	}
+	
+	public void test(){
+		MongoDatabase dbtest = mongoClient.getDatabase("BDMongomaster041");
+		MongoCollection<Document> collection = dbtest.getCollection("oeuvres");
+		Document myDoc = collection.find().first();
+		System.out.println(myDoc);
+	}
+	public void creation(){
+		
 	}
 }
---
+++ project_poc_2017/src/com/project/poc/shared/bean/HistoriqueConnexion.java
@@ -0,0 +1,58 @@
+package com.project.poc.shared.bean;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.project.poc.shared.annotation.Id;
+import com.project.poc.shared.annotation.Table;
+
+@Table(name="t_connexions")
+public class HistoriqueConnexion {
+	@Id
+	private double idConnexion;
+	private double idUtilisateur;
+	private String systeme;
+	private String navigateur;
+	private Date dateConnexion ;
+	private Date dateDeconnexion;
+	private Map<Date,String> pagesVisitées;
+	
+	public HistoriqueConnexion(double idConnexion, double idUtilisateur, String systeme, String navigateur, Date dateConnexion) {
+		super();
+		this.idConnexion = idConnexion;
+		this.idUtilisateur = idUtilisateur;
+		this.systeme = systeme;
+		this.navigateur = navigateur;
+		this.dateConnexion = dateConnexion;
+		this.dateDeconnexion = null;
+		this.pagesVisitées = new HashMap<Date,String>();
+	}
+	public Date getDateDeconnexion() {
+		return dateDeconnexion;
+	}
+	public void setDateDeconnexion(Date dateDeconnexion) {
+		this.dateDeconnexion = dateDeconnexion;
+	}
+	public Map<Date, String> getPagesVisitées() {
+		return pagesVisitées;
+	}
+	public void addPagesVisitées(Date date, String page) {
+		this.pagesVisitées.put(date, page);
+	}
+	public double getIdConnexion() {
+		return idConnexion;
+	}
+	public double getIdUtilisateur() {
+		return idUtilisateur;
+	}
+	public String getSysteme() {
+		return systeme;
+	}
+	public String getNavigateur() {
+		return navigateur;
+	}
+	public Date getDateConnexion() {
+		return dateConnexion;
+	}
+}
