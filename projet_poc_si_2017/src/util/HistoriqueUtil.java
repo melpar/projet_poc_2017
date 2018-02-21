d70bec2bcb92897c0c64b940b4366ee66ecd2baf
thomas <thomas@DESKTOP-DNRT0HT>
2018-02-21 23:36:05 +0100

    [HistoriqueUtil] réparation des erreurs

--- projet_poc_si_2017/src/util/HistoriqueUtil.java
+++ projet_poc_si_2017/src/util/HistoriqueUtil.java
@@ -10,44 +10,80 @@ import bean.mongdb.HistoriqueConnexion;
 public class HistoriqueUtil {
 	int idConnexion;
 
+	public HistoriqueUtil(HttpSession session) {
+		Date dateConnexion = new Date();
+		BaseMongoDB mongo = new BaseMongoDB();
+		mongo.ouvrir();
+		idConnexion = mongo.createIdConnexion();
+		String systeme = System.getProperties().getProperty("os.name");
+		// String navigateur = request.getHeader("User-Agent");
+		String navigateur = null;
+		HistoriqueConnexion historique = new HistoriqueConnexion(idConnexion, systeme, navigateur, dateConnexion);
+		mongo.ajoutHistoriqueConnexion(historique);
+		mongo.fermer();
+	}
+
 	public static HistoriqueUtil creer(HttpSession session) {
-		HistoriqueUtil histo = session.getAttribute("histoUtil");
+		HistoriqueUtil histo = (HistoriqueUtil) session.getAttribute("histoUtil");
 		if (histo == null) {
 			// si mail == null => génération d'id
-			histo = new HistoriqueUtil(session.getAttribute("mail"));
+			// if(session.getAttribute("mail") == null)
+			// histo = new HistoriqueUtil(session.getAttribute("mail"));
+			histo = new HistoriqueUtil(session);
 			session.setAttribute("", histo);
 		}
 		return histo;
 	}
 
-	public void historique(String url, String systeme, String navigateur) {
-		// String url = request.getParameter("url");
+	public void setIdUtilisateur(int idUtilisateur) {
+
+		BaseMongoDB mongo = new BaseMongoDB();
+		mongo.ouvrir();
+		mongo.updateConnexion(idConnexion, idUtilisateur);
+		mongo.fermer();
+	}
+
+	public void addPageHistorique(String url) {
 		Date date = new Date();
 		BaseMongoDB mongo = new BaseMongoDB();
 		mongo.ouvrir();
-		// TODO : à rajouter dans la JSP mongo.createIdConnexion(),
-		int idConnexion;
-		if (request.getSession().getAttribute("idConnexion") == null) {
-			request.getSession().setAttribute("idConnexion", mongo.createIdConnexion());
-			idConnexion = (int) request.getSession().getAttribute("idConnexion");
-			if (request.getSession().getAttribute("idUtilisateur") == null) {
-				String systeme = System.getProperties().getProperty("os.name");
-				String navigateur = request.getHeader("User-Agent");
-				HistoriqueConnexion historique = new HistoriqueConnexion(idConnexion, systeme, navigateur, date);
-				mongo.ajoutHistoriqueConnexion(historique);
-			} else {
-				int idUtilisateur = (int) request.getSession().getAttribute("idConnexion");
-				String systeme = System.getProperties().getProperty("os.name");
-				String navigateur = request.getHeader("User-Agent");
-				HistoriqueConnexion historique = new HistoriqueConnexion(idConnexion, idUtilisateur, systeme,
-						navigateur, date);
-				mongo.ajoutHistoriqueConnexion(historique);
-			}
-		} else {
-			idConnexion = (int) request.getSession().getAttribute("idConnexion");
-		}
 		mongo.updateConnexion(idConnexion, date, url);
 		mongo.fermer();
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	public void setDeconnexion() {
+		Date date = new Date();
+		BaseMongoDB mongo = new BaseMongoDB();
+		mongo.ouvrir();
+		mongo.updateConnexion(idConnexion, date);
+		mongo.fermer();
+	}
+
+	/*
+	 * public void historique(String url, String systeme, String navigateur) { //
+	 * String url = request.getParameter("url"); Date date = new Date(); BaseMongoDB
+	 * mongo = new BaseMongoDB(); mongo.ouvrir(); // TODO : à rajouter dans la JSP
+	 * mongo.createIdConnexion(), int idConnexion; if
+	 * (request.getSession().getAttribute("idConnexion") == null) {
+	 * request.getSession().setAttribute("idConnexion", mongo.createIdConnexion());
+	 * idConnexion = (int) request.getSession().getAttribute("idConnexion"); if
+	 * (request.getSession().getAttribute("idUtilisateur") == null) { String systeme
+	 * = System.getProperties().getProperty("os.name"); String navigateur =
+	 * request.getHeader("User-Agent"); HistoriqueConnexion historique = new
+	 * HistoriqueConnexion(idConnexion, systeme, navigateur, date);
+	 * mongo.ajoutHistoriqueConnexion(historique); } else { int idUtilisateur =
+	 * (int) request.getSession().getAttribute("idConnexion"); String systeme =
+	 * System.getProperties().getProperty("os.name"); String navigateur =
+	 * request.getHeader("User-Agent"); HistoriqueConnexion historique = new
+	 * HistoriqueConnexion(idConnexion, idUtilisateur, systeme, navigateur, date);
+	 * mongo.ajoutHistoriqueConnexion(historique); } } else { idConnexion = (int)
+	 * request.getSession().getAttribute("idConnexion"); }
+	 * mongo.updateConnexion(idConnexion, date, url); mongo.fermer();
+	 * response.getWriter().append("Served at: ").append(request.getContextPath());
+	 * }
+	 */
+
+	int getIdUtilisateur() {
+		return 0;
 	}
 }
